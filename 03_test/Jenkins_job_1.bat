cb5201e69c7a942ca37308770d80557d27f1d30f
Miroslava <miroslavaroshchina@gmail.com>
2021-11-05 19:49:12 +0200

    ver 2

--- 03_test/Jenkins_job_1.bat
+++ 03_test/Jenkins_job_1.bat
@@ -1,2 +1,3 @@
-C:\Users\asus\TestEnv\Scripts\activate.bat
+REM C:\Users\asus\TestEnv\Scripts\activate.bat
+python -m pylint "D:VUM STUDY\2 year\1 semester\Software Metrics - Tools and Methodology\2. Homework\Python\03_test\_03_test.py"
 C:\Users\asus\AppData\Local\Programs\Python\Python310\python.exe "D:VUM STUDY\2 year\1 semester\Software Metrics - Tools and Methodology\2. Homework\Python\03_test\_03_test.py"
