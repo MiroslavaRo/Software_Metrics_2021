380e9e40b3d33508711622533cafdb7b1da087e0
Miroslava <miroslavaroshchina@gmail.com>
2021-11-05 19:29:32 +0200

    add jenkins

---
+++ 03_test/Jenkins_job_1.bat
@@ -0,0 +1,2 @@
+C:\Users\asus\TestEnv\Scripts\activate.bat
+C:\Users\asus\AppData\Local\Programs\Python\Python310\python.exe "D:VUM STUDY\2 year\1 semester\Software Metrics - Tools and Methodology\2. Homework\Python\03_test\_03_test.py"
